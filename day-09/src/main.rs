1fa667f0459e13895b60dad526b459578944b0dd
Kanjirito <kanjirito@protonmail.com>
2021-12-13 17:58:29 +0100

    Day 9 improvement
    Surrounded the input of day 9 with "9" to avoid having to check boundaries.
    "9" can't be the lowest point so you can safely just pass checking it.
    This will help with part 2.

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -10,17 +10,12 @@ fn main() {
     let mut counter: usize = 0;
     for (x, column) in input.iter().enumerate() {
         'number: for (y, number) in column.iter().enumerate() {
+            if number == &9 {
+                continue;
+            }
             for x_offset in [-1, 0, 1] {
                 for y_offset in [-1, 0, 1] {
-                    if x_offset == 0 && y_offset == 0 {
-                        continue;
-                    } else if x == 0 && x_offset == -1 {
-                        continue;
-                    } else if x == input.len() - 1 && x_offset == 1 {
-                        continue;
-                    } else if y == 0 && y_offset == -1 {
-                        continue;
-                    } else if y == column.len() - 1 && y_offset == 1 {
+                    if x_offset == y_offset {
                         continue;
                     } else if number
                         >= &input[(x as isize + x_offset) as usize]
@@ -42,7 +37,11 @@ fn load_input() -> Vec<Vec<usize>> {
     let reader = BufReader::new(file);
     let mut input: Vec<Vec<usize>> = Vec::new();
     for line in reader.lines() {
-        input.push(line.unwrap().split("").flat_map(|x| x.parse()).collect());
+        let line: String = format!("{}{}{}", 9, line.unwrap(), 9);
+        input.push(line.split("").flat_map(|x| x.parse()).collect());
     }
+    let l = input[0].len();
+    input.insert(0, vec![9; l]);
+    input.push(vec![9; l]);
     input
 }

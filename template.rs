965dedc36c91473917d452262027a00439159ef9
Kanjirito <kanjirito@protonmail.com>
2022-12-12 17:44:09 +0100

    Add example test to template

--- template.rs
+++ template.rs
@@ -3,10 +3,15 @@ use std::io::prelude::*;
 use std::io::BufReader;
 
 fn main() {
-    let input = load_input();
+    let input = load_input("input");
 }
 
-fn load_input() {
-    let file = File::open("input").expect("No input file found");
+fn load_input(name: &str) {
+    let file = File::open(name).expect("No input file found");
     let reader = BufReader::new(file);
+}
+
+#[test]
+fn example() {
+    let input = load_input("example");
 }
